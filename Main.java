759c4991cf1ee93636a3657e9ca353ecf1774146
Graham Koenig <89868570+Koeniggw@users.noreply.github.com>
2021-09-21 19:59:03 +0000

    Should be finalized

--- Main.java
+++ Main.java
@@ -11,20 +11,23 @@ class Main {
     Random r = new Random();
     int randomNum = r.nextInt(50) + 1;
     System.out.println("The Random number is: " + randomNum);
+
     if (randomNum <= 5) {
       while (randomNum >= 0) {
         System.out.println(randomNum);
         randomNum--;
       }
       System.out.println("Ahoy mateys!");
-    } else if (randomNum > 25 && randomNum < 42) {
+    } 
+    
+    else if (randomNum > 25 && randomNum < 42) {
       while (randomNum >= 0) {
         System.out.println(randomNum);
         randomNum--;
       }
-
       System.out.println("Cannonball!");
     }
+
     else
     {
       while (randomNum >= 0) {
